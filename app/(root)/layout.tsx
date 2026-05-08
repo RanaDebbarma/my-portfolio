ee4afe71607e130132672eeb0d6c70238ec75a79
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-05-09 04:35:13 +0530

    nav style

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,9 +2,19 @@ import NavBar from "@/components/NavBar"
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      <NavBar />
-      {children}
+    <div className="mx-auto mt-2 mb-2 max-w-7xl w-[95%] flex flex-col gap-2">
+      {/* navigation */}
+      <div className="border border-white/50 rounded-3xl px-4 py-3 flex justify-between items-center">
+        <div className="text-3xl">
+          <span className="text-red-400">R </span>
+          <span className="text-blue-400">D </span>
+        </div>
+        <NavBar />
+      </div>
+      {/* content */}
+      <div>
+        {children}
+      </div>
     </div>
   )
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import { motion } from "motion/react";
 import Link from "next/link";
+import { useState } from "react";
 
 const navItems = [
   { name: "home", href: "/" },
@@ -12,11 +14,36 @@ const navItems = [
 
 
 const NavBar = () => {
+  const [activeTab, setActiveTab] = useState(navItems[0].name)
+
   return (
-    <div>
+    <div className="w-fit border border-white/50 rounded-3xl flex gap-1 p-1">
+      {/* NavTabs */}
       {navItems.map(nav => (
-        <Link key={nav.href} href={nav.href}>
-          {nav.name}
+        <Link
+          key={nav.href}
+          href={nav.href}
+          onClick={() => setActiveTab(nav.name)}
+          className={`${activeTab === nav.name
+            ? '' : 'hover:text-white/50'
+            }  relative`}
+        >
+          {/* pill */}
+          {activeTab === nav.name &&
+            <motion.div
+              layoutId="active-pill"
+              className="absolute inset-0 z-10 bg-stone-300 mix-blend-difference"
+              style={{
+                borderRadius: 9999
+              }}
+            // transition={{duration: 5}}
+            />
+          }
+
+          {/* text */}
+          <div className="px-3 py-1">
+            {nav.name}
+          </div>
         </Link>
       ))}
     </div>
