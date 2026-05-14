fb29cdd9060a8435f0f75622ce76a1e966db9d37
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-05-15 04:41:44 +0530

    Home update

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,11 @@
 const Logo = () => {
   return (
-    <div className="text-2xl font-serif font-bold p-2 border-2 border-foreground/5 concaveRidge rounded-full flex gap-1">
-      <span className="mix-blend-difference text-emerald-500">R</span>
-      <span className="mix-blend-difference text-emerald-500">D</span>
+    <div 
+      aria-label="RD Logo"
+      className="select-none text-2xl font-serif font-bold p-2 border-2 border-foreground/5 concaveRidge rounded-full flex gap-1"
+    >
+      <span className="text-accent text-shadow-md">R</span>
+      <span className="text-accent text-shadow-md">D</span>
     </div>
   )
 }
