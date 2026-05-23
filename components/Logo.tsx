cfb99a0e717b1bef722cc5f563801bfc1ab088a5
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-05-24 05:07:00 +0530

    Nav design changes

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ const Logo = () => {
   return (
     <div 
       aria-label="RD Logo"
-      className="select-none text-2xl items-center justify-center font-serif font-bold p-2 border-2 border-foreground/5 concaveRidge rounded-4xl flex gap-1"
+      className="convex select-none h-full aspect-auto text-lg items-center justify-center font-serif font-bold p-2 border border-foreground/20 rounded-full flex gap-1"
     >
       <span className="text-accent text-shadow-md">R</span>
       <span className="text-accent text-shadow-md">D</span>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -18,7 +18,7 @@ const NavBar = () => {
   const activeTab = navItems.find(nav => nav.href === pathname)?.name || navItems[0].name;
 
   return (
-    <div className="isolate w-fit border-3 border-secondary bg-background concaveRidge rounded-4xl flex justify-center items-center flex-wrap gap-1 p-2">
+    <div className="concaveRidge isolate w-fit border-2 border-foreground/10 bg-background rounded-4xl flex justify-center items-center flex-wrap gap-1 p-2 overflow-hidden">
       {/* NavTabs */}
       {navItems.map(nav => (
         <Link
