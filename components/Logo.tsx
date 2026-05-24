2736a43a367ca87a91c8baa74162e7ddc8560f04
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-05-24 05:54:02 +0530

    nav element adjustment

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ const Logo = () => {
   return (
     <div 
       aria-label="RD Logo"
-      className="convex select-none h-full aspect-auto text-lg items-center justify-center font-serif font-bold p-2 border border-foreground/20 rounded-full flex gap-1"
+      className="convex select-none h-12 w-12 aspect-auto text-lg items-center justify-center font-serif font-bold p-2 border border-foreground/20 rounded-full flex gap-1"
     >
       <span className="text-accent text-shadow-md">R</span>
       <span className="text-accent text-shadow-md">D</span>
--- components/Overview.tsx
+++ components/Overview.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const Overview = () => {
   return (
-    <div className="shadow-[0_4px_24px_rgba(0,0,0,0.24)] flex flex-col gap-12 lg:w-120 border border-foreground/16 bg-foreground/5 w-full rounded-3xl shrink-0 p-8">
+    <div className="shadow-[0_4px_24px_rgba(0,0,0,0.24)] flex flex-col gap-12 lg:w-120 border border-foreground/16 bg-foreground/5 w-full rounded-3xl shrink-0 p-4 sm:p-8">
       <h1 className="uppercase tracking-widest">Quick Overview</h1>
 
       <div>
@@ -11,7 +11,7 @@ const Overview = () => {
         <p className="text-muted">Frontend Developer based in Guwahati</p>
       </div>
       
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div className="space-y-2 p-8 border-0 border-foreground/30 concave rounded-2xl">
           <p className="uppercase text-xs tracking-widest font-sans font-medium text-muted">Primary stack</p>
           <p>React + tailwind</p>
