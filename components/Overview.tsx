3c3ae8745bdd9f3679483f2736abcb3b95284d33
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-05-27 02:51:44 +0530

    animate project in overview

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -1,8 +1,38 @@
+"use client"
+
 import { ProfileData } from "@/lib/data"
 import Image from "next/image"
 import { TiltCard } from "./tilt-card"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import { AnimatePresence, motion } from "motion/react"
 
 const Overview = () => {
+  const router = useRouter();
+  const projects = ProfileData.projects.cards;
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [projectData, setProjectData] = useState<{ title: string, image: string }>(
+    {
+      title: projects[0]?.title || "Loading...",
+      image: projects[0]?.image
+    }
+  );
+  const delay = 3.14;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const nextIndex = (currentIndex + 1) % projects.length;
+
+      // Update index and data
+      setCurrentIndex(nextIndex);
+      setProjectData(projects[nextIndex]);
+
+    }, delay * 1000);
+
+    return () => clearInterval(interval);
+  }, [projects, currentIndex]);
+
   return (
     <div className="relative shadow-[0_4px_24px_rgba(0,0,0,0.24)] flex flex-col gap-6 md:w-100 lg:w-120 h-fit border border-wrapper-border/30 bg-secondary w-full rounded-3xl shrink-0 p-6 sm:p-8 transition-all duration-300">
       {/* title */}
@@ -46,22 +76,41 @@ const Overview = () => {
       </div>
 
       {/* Projects */}
-      <h4 className="uppercase tracking-widest font-sans">Project</h4>
+      <p className="uppercase tracking-widest font-sans">Project</p>
       <div className="flex-1 flex flex-col justify-center items-center">
-        <div className="rounded-2xl">
+        <div className="w-full rounded-2xl">
           {/* project title */}
-          <h4 className="border-t border-wrapper-border/30 py-2 w-full text-center font-sans tracking-wide text-muted rounded-2xl">
-            Kanban board
-          </h4>
-          {/* project image */}
-          <div className="overflow-hidden border border-wrapper-border/30 rounded-2xl">
-            <Image
-              className="hover:scale-110 transition-transform duration-800 w-full h-auto object-cover rounded-2xl"
-              src="/assets/projects/KanbanBoard.png"
-              alt="kanban-board"
-              width={500}
-              height={500}
-            />
+          <motion.div>
+            <p className="border-t border-wrapper-border/30 py-2 w-full text-center font-sans tracking-wide text-muted rounded-2xl">
+              {projectData.title}
+            </p>
+          </motion.div>
+          <div className="relative border border-wrapper-border/30 rounded-2xl h-56 w-full overflow-hidden">
+            <AnimatePresence>
+              <motion.div
+                className="absolute inset-0"
+                key={projectData.title}
+                initial={{ opacity: 0, x: -200 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: 200 }}
+                transition={{
+                  duration: 0.6,
+                  ease: "easeInOut",
+                }}
+              >
+                {/* project image */}
+                <div className="overflow-hidden">
+                  <Image
+                    onClick={() => router.push('/projects')}
+                    className="cursor-pointer hover:scale-110 transition-transform duration-800 w-full h-auto object-cover"
+                    src={projectData.image}
+                    alt={projectData.title}
+                    width={500}
+                    height={500}
+                  />
+                </div>
+              </motion.div>
+            </AnimatePresence>
           </div>
         </div>
       </div>
