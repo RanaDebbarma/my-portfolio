03d33bd59756ff789d3b85157bfbf62695150875
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-05-28 02:33:51 +0530

    animate logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,18 @@
+import { TiltCard } from "./tilt-card"
+
 const Logo = () => {
   return (
-    <div 
+    <div
       aria-label="RD Logo"
-      className="convex bg-[#171717] select-none h-12 w-12 aspect-auto text-lg items-center justify-center font-serif font-bold p-2 border border-white/20 rounded-full flex gap-1"
+      className="convex bg-[#171717] select-none h-12 w-12 aspect-auto text-lg items-center justify-center font-serif font-bold border border-white/20 rounded-full flex gap-1"
     >
-      <span className="text-accent font-bold">RD</span>
-      {/* <span className="text-accent font-extrabold font-sans">D</span> */}
+      <TiltCard
+        strength={-16}
+        className="h-full w-full p-2 rounded-full"
+      >
+        <span className="text-accent font-bold">RD</span>
+        {/* <span className="text-accent font-extrabold font-sans">D</span> */}
+      </TiltCard>
     </div>
   )
 }
