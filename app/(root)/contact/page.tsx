2fcdb31123effa0f81ed2904b6f54035440e37c1
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-05-27 07:48:59 +0530

    contact page

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -1,9 +1,11 @@
+import { TiltCard } from "@/components/tilt-card"
 import CustomHR from "@/components/ui/CustomTags"
 import { ProfileData } from "@/lib/data"
+import Link from "next/link"
 
 const contact = () => {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 min-h-full">
       {/* Introduction */}
       <div className="space-y-2 p-4 bg-">
         <h3 className="tracking-widest uppercase font-semibold font-sans text-muted">Contact</h3>
@@ -13,6 +15,31 @@ const contact = () => {
         </div>
       </div>
       <CustomHR />
+      {/* Contact Information */}
+      <div className="grow flex flex-col justify-center items-center p-4">
+        <div className="grid grid-cols-2 sm:grid-cols-2 gap-8">
+          {ProfileData.contact.cards.map((item, index) => (
+            <TiltCard
+              key={index}
+              strength={20}
+              className="relative group border border-wrapper-border/30 hover:border-accent bg-secondary hover:bg-accent/2 rounded-2xl px-6 py-5"
+            >
+              <Link
+                href={item.contact}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex flex-col justify-center items-center gap-2 group-hover:scale-105 transition-transform duration-300"
+                >
+                <div className="h-10 p-2 border border-wrapper-border/50 group-hover:border-accent/80 bg-background rounded-lg fill-foreground group-hover:fill-accent">{item.icon}</div>
+                <p
+                  className="text-lg font-sans font-extralight tracking-wider text-foreground group-hover:text-accent"
+                >
+                  {item.title}</p>
+              </Link>
+            </TiltCard>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
